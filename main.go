3b6ad0f9f54e2b8f27ff5d64ad98fcf1b0569623
George Spanos <gespa11019@gmail.com>
2022-12-10 13:13:30 +0200

    greet change

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ func main() {
 }
 func run() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/greet", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello stranger",
+			"message": "Hello stranger from dockerhub",
 		})
 	})
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.File("./static/index.html")
 	})
-	r.Use(cors.Default())
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
